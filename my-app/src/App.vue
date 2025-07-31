cfc00452e5952346c30fd1cb0fbd8c4958d939cc
kuwabarbara <nobuaki.kuwabara3@gmail.com>
2025-08-01 03:28:58 +0900

    若干UIを変更

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,16 +1,15 @@
 <!-- src/App.vue -->
 <template>
-  <nav class="p-4 bg-gray-100 space-x-4">
+  <nav class="p-4 bg-gray-100 flex space-x-4">
     <router-link to="/"        class="text-blue-600 hover:underline">投票ページ</router-link>
-    <router-link to="/ranking" class="text-blue-600 hover:underline">ランキング</router-link>
-  </nav>
+</nav>
 
-  <!-- ここがないと、どのページも表示されません -->
+  <nav class="p-4 bg-gray-100 flex space-x-4">
+    <router-link to="/ranking" class="text-blue-600 hover:underline">ランキング</router-link>
+</nav>
+ 
+
   <main class="p-4">
     <router-view />
   </main>
 </template>
-
-<script setup>
-// nothing needed here
-</script>
