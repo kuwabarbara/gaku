e0f8407de78abf76e1df3e92f923d79c088f6675
kuwabarbara <nobuaki.kuwabara3@gmail.com>
2025-08-02 02:06:38 +0900

    学歴の種類を追加

--- my-app/src/data/entries.js
+++ my-app/src/data/entries.js
@@ -1,8 +1,79 @@
 // src/data/entries.js
-
+/*
 export const entries = [
   { id: "utokyo_lit", name: "東京大学 文学部" },
   { id: "keio_law", name: "慶應義塾大学 法学部" },
   { id: "waseda_comm", name: "早稲田大学 商学部" },
   { id: "kyoto_sci", name: "京都大学 理学部" }
+];
+*/
+
+// src/data/entries.js
+
+export const entries = [
+{ id: "utokyo\_lit1", name: "東京大学 文科一類" },
+{ id: "utokyo\_lit2", name: "東京大学 文科二類" },
+{ id: "utokyo\_lit3", name: "東京大学 文科三類" },
+{ id: "utokyo\_sci1", name: "東京大学 理科一類" },
+{ id: "utokyo\_sci2", name: "東京大学 理科二類" },
+{ id: "utokyo\_sci3", name: "東京大学 理科三類" },
+
+{ id: "kyoto\_lit", name: "京都大学 文学部" },
+{ id: "kyoto\_edu", name: "京都大学 教育学部" },
+{ id: "kyoto\_law", name: "京都大学 法学部" },
+{ id: "kyoto\_econ", name: "京都大学 経済学部" },
+{ id: "kyoto\_sci", name: "京都大学 理学部" },
+{ id: "kyoto\_eng", name: "京都大学 工学部" },
+{ id: "kyoto\_agri", name: "京都大学 農学部" },
+
+{ id: "osaka\_human", name: "大阪大学 人間科学部" },
+{ id: "osaka\_law", name: "大阪大学 法学部" },
+{ id: "osaka\_econ", name: "大阪大学 経済学部" },
+{ id: "osaka\_sci", name: "大阪大学 理学部" },
+{ id: "osaka\_eng", name: "大阪大学 工学部" },
+{ id: "osaka\_med", name: "大阪大学 医学部 医学科" },
+
+{ id: "tohoku\_law", name: "東北大学 法学部" },
+{ id: "tohoku\_ed", name: "東北大学 教育学部" },
+{ id: "tohoku\_sci", name: "東北大学 理学部" },
+{ id: "tohoku\_med", name: "東北大学 医学部 医学科" },
+{ id: "tohoku\_dent", name: "東北大学 歯学部" },
+
+{ id: "nagoya\_law", name: "名古屋大学 法学部" },
+{ id: "nagoya\_econ", name: "名古屋大学 経済学部" },
+{ id: "nagoya\_eng", name: "名古屋大学 工学部" },
+{ id: "nagoya\_med", name: "名古屋大学 医学部 医学科" },
+
+{ id: "hokkaido\_law", name: "北海道大学 法学部" },
+{ id: "hokkaido\_sci", name: "北海道大学 理学部" },
+{ id: "hokkaido\_agri", name: "北海道大学 農学部" },
+{ id: "hokkaido\_med", name: "北海道大学 医学部 医学科" },
+
+{ id: "keio\_law", name: "慶應義塾大学 法学部" },
+{ id: "keio\_comm", name: "慶應義塾大学 商学部" },
+{ id: "keio\_env", name: "慶應義塾大学 環境情報学部" },
+{ id: "keio\_sfc", name: "慶應義塾大学 総合政策学部" },
+{ id: "keio\_med", name: "慶應義塾大学 医学部" },
+
+{ id: "waseda\_law", name: "早稲田大学 法学部" },
+{ id: "waseda\_comm", name: "早稲田大学 商学部" },
+{ id: "waseda\_sports", name: "早稲田大学 スポーツ科学部" },
+{ id: "waseda\_int", name: "早稲田大学 国際教養学部" },
+
+{ id: "hitotsubashi\_law", name: "一橋大学 法学部" },
+{ id: "hitotsubashi\_comm", name: "一橋大学 商学部" },
+{ id: "hitotsubashi\_soc", name: "一橋大学 社会学部" },
+{ id: "hitotsubashi\_econ", name: "一橋大学 経済学部" },
+
+{ id: "kobe\_law", name: "神戸大学 法学部" },
+{ id: "kobe\_econ", name: "神戸大学 経済学部" },
+{ id: "kobe\_bus", name: "神戸大学 経営学部" },
+{ id: "kobe\_mar", name: "神戸大学 海事科学部" },
+
+{ id: "hiroshima\_eng4", name: "広島大学 工学部 第四類 建築学科" },
+{ id: "hiroshima\_med", name: "広島大学 医学部 医学科" },
+{ id: "chiba\_med", name: "千葉大学 医学部 医学科" },
+{ id: "yokohama\_data", name: "横浜国立大学 データサイエンス学部" },
+{ id: "tsukuba\_info", name: "筑波大学 情報学群" },
+{ id: "tsukuba\_med", name: "筑波大学 医学群 医学類" },
 ];
